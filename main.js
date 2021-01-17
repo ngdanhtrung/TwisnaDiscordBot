c4d98a20566efaeb069b58a90dc60a46ac8b4c5d
NGUYỄN DANH TRUNG <4501104260@student.hcmue.edu.vn>
2021-01-18 04:31:21 +0700

    Add main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('TwisnaBot is online!');
+})
+
+
+
+
+
+
+
+
+client.login('ODAwNDczOTY2MTE2NDcwODM0.YASpeQ.MZJGf7SBmcA9cb0iL0J1Q2HGccs');
