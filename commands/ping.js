652e0cc72965dd407f0b662ff033f9cdcaca5fb9
NGUYỄN DANH TRUNG <4501104260@student.hcmue.edu.vn>
2021-02-12 02:51:11 +0700

    Update ping command

--- commands/ping.js
+++ commands/ping.js
@@ -3,6 +3,8 @@ module.exports = {
     aliases: [],
     description: "this is a ping command!",
     execute(client, message, args) {
-        message.channel.send("Pong!");
+        // message.channel.send("Pong!");
+        // message.channel.send(`🏓Latency is ${Date.now() - message.createdTimestamp}ms`);
+        message.channel.send(`Latency is ${Date.now() - message.createdTimestamp}ms\nAPI Latency is ${Math.round(client.ws.ping)}ms`);
     },
 };
