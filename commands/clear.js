be9bcd1f2d8d6067e7aed80ee28e755358021a94
NGUYỄN DANH TRUNG <4501104260@student.hcmue.edu.vn>
2021-02-13 17:32:43 +0700

    Updated for clear command

--- commands/clear.js
+++ commands/clear.js
@@ -1,13 +1,19 @@
 module.exports = {
     name: "clear",
     aliases: ["purge"],
+    permissions: ["ADMINISTRATOR", "MANAGE_MESSAGES"],
     description: "this is a clear command!",
     async execute(client, message, args) {
-        if (!args[0]) return message.reply("Please enter the amount of messages that you want to clear!");
+        if (!args[0])
+            return message.reply(
+                "Please enter the amount of messages that you want to clear!"
+            );
         if (isNaN(args[0])) return message.reply("Please enter a number!");
 
-        if (args[0] > 100) return message.reply("You can't delete more than 100 messages");
-        if (args[0] < 1) return message.reply("You must delete at least one message!");
+        if (args[0] > 100)
+            return message.reply("You can't delete more than 100 messages");
+        if (args[0] < 1)
+            return message.reply("You must delete at least one message!");
 
         await message.channel.messages
             .fetch({ limit: ++args[0] })
