d3fe9d758a31c9934e52fe4d6e4645530d8071e5
NGUYỄN DANH TRUNG <4501104260@student.hcmue.edu.vn>
2021-02-17 01:54:07 +0700

    Updated for invite command

--- commands/invite.js
+++ commands/invite.js
@@ -4,21 +4,27 @@ module.exports = {
     permissions: ["SEND_MESSAGES"],
     description: "send invte link of BOT",
     execute(client, message, args, Discord) {
+        const avatarURL = client.user.displayAvatarURL({
+            format: "png",
+            dynamic: true,
+            size: 1024,
+        });
+        const inviteURL = "https://discord.com/oauth2/authorize?client_id=800473966116470834&scope=bot&permissions=8";
         const newEmbed = new Discord.MessageEmbed()
             .setColor("#F4D18F")
             .setTitle("Hi! I'm Twisna Bot!")
             .setURL(
-                "https://discord.com/oauth2/authorize?client_id=800473966116470834&scope=bot&permissions=8"
+                inviteURL
             )
             .setAuthor(
                 "Twisna Bot!",
-                "https://media.discordapp.net/attachments/811298966293577729/811298986635558912/tohru-drunk.jpg?width=676&height=676"
+                avatarURL
             )
             .setDescription(
-                "Nice to meet you! Here is my [invite link](https://discord.com/oauth2/authorize?client_id=800473966116470834&scope=bot&permissions=8)"
+                `Nice to meet you! Here is my [invite link](${inviteURL})`
             )
             .setThumbnail(
-                "https://media.discordapp.net/attachments/811298966293577729/811298986635558912/tohru-drunk.jpg?width=676&height=676"
+                avatarURL
             );
         message.channel.send(newEmbed);
     },
